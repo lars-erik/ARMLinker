f62320ea78153b69a6a2a1ccbfb958a9cde7a281
Lars-Erik Aabech <lea@markedspartner.no>
2020-01-13 09:52:25 +0100

    Adding powershell module

---
+++ src/ARMLinker.PowerShell/ARMLinker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARMLinker.PowerShell
+{
+    [Cmdlet("Convert", "TemplateLinks")]
+    public class ARMLinker : Cmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string InputPath { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var fullPath = Path.GetFullPath(InputPath);
+            var linker = new ArmJsonLinker()
+
+            
+            base.ProcessRecord();
+        }
+    }
+}
